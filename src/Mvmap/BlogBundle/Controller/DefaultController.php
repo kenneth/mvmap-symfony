2110cb589cd0a4f0a0a6f3dfd611ab2fba694caa
kenneth <kenneth1982@gmail.com>
2014-12-12 19:37:32 +0800

    study router

--- src/Mvmap/BlogBundle/Controller/DefaultController.php
+++ src/Mvmap/BlogBundle/Controller/DefaultController.php
@@ -3,11 +3,22 @@
 namespace Mvmap\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('MvmapBlogBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function testAction($name)
+    {
+        return $this->render('MvmapBlogBundle:Default:test.html.twig', array('name' => $name));
+    }
+    
+    public function helloAction()
+    {
+        return new Response('Hello world!');
     }
 }
