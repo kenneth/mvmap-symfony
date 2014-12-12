4246631695c2152cb91fd0c19b288a81be26502d
kenneth <kenneth1982@gmail.com>
2014-12-12 19:10:21 +0800

    add admin bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Mvmap\BlogBundle\MvmapBlogBundle(),
             new Mvmap\UserBundle\MvmapUserBundle(),
+            new Mvmap\AdminBundle\MvmapAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Mvmap/AdminBundle/MvmapAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Mvmap\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MvmapAdminBundle extends Bundle
+{
+}
